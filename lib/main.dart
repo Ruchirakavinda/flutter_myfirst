180d2694a2105cfb08912afb88c7cd13d9498399
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-06-30 00:58:27 +0530

    News feed modified01

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -115,109 +117,392 @@ class MyHome extends StatelessWidget {
 
 // ignore: non_constant_identifier_names
 Widget Tab1(){
-  return Container(
-    child: Column(
-      children: [
-
-        //ROW01
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:<Widget> [
-            Column(
-              children:<Widget> [
-                Container(
-                  margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                  child:Text(
-                     "News Feed",
-                     style: TextStyle(
-                       fontSize:25.0 ,fontWeight: FontWeight.w700,
-                       ),
-                       )
-                       ),
-              ],
-            ),
-
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: Column(
+  return SingleChildScrollView(
+    child: Container(
+      child: Column(
+        children: [
+  
+          //ROW01
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:<Widget> [
+              Column(
                 children:<Widget> [
-                  FloatingActionButton(
-                    backgroundColor: Colors.grey.withOpacity(0.2),
-                    mini: true,
-                    elevation: 0.0,
-                    child: Icon(Icons.search,size: 25,color: Colors.black,),
-                    onPressed: (){},
-                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child:Text(
+                       "News Feed",
+                       style: TextStyle(
+                         fontSize:25.0 ,fontWeight: FontWeight.w700,
+                         ),
+                         )
+                         ),
                 ],
               ),
-            ),
-          ],
-        ),
-        Divider(
-                  color: Colors.black.withOpacity(0.1),
-                  height: 0,
-                  thickness: 3,
-                  indent: 20,
-                  endIndent: 20,
-                
+  
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: Column(
+                  children:<Widget> [
+                    FloatingActionButton(
+                      backgroundColor: Colors.grey.withOpacity(0.2),
+                      mini: true,
+                      elevation: 0.0,
+                      child: Icon(Icons.search,size: 25,color: Colors.black,),
+                      onPressed: (){},
+                    ),
+                  ],
                 ),
-        //ROW02
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:<Widget> [
+              ),
+            ],
+          ),
+          Divider(
+                    color: Colors.black.withOpacity(0.1),
+                    height: 0,
+                    thickness: 3,
+                    indent: 20,
+                    endIndent: 20,
+                  
+                  ),
+          //ROW02
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:<Widget> [
+              Column(
+                children: [
+                  Row(
+                    children:<Widget> [
+                      Container(
+                      width: 40,
+                      height: 40,
+                      margin: EdgeInsets.fromLTRB(20,15,0,0),
+                      decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                            "assests/girl.png"),
+                        fit: BoxFit.fill,
+                      ),
+                      ),
+                    ),
+  
+                    Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 0,0),
+                        child: Text("Sunny Leony",style: TextStyle(
+                          fontSize:18.0 ,fontWeight: FontWeight.w700,)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 0,0),
+                        child: Text("Just Now..",style: TextStyle(
+                          fontSize:15.0 ,)),
+                      )
+                    ],
+                    ),
+                   ],
+                 ),
+                ],
+              ),
+  
             Column(
               children: [
-                Row(
-                  children:<Widget> [
-                    Container(
-                    width: 40,
-                    height: 40,
-                    margin: EdgeInsets.fromLTRB(20,15,0,0),
-                    decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage(
-                          "assests/girl.png"),
-                      fit: BoxFit.fill
-                    ),
-                    ),
-                  ),
-
-                  Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 10, 0,0),
-                      child: Text("Ruchira Kavinda",style: TextStyle(
-                        fontSize:18.0 ,fontWeight: FontWeight.w700,)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 0, 0,0),
-                      child: Text("Just Now..",style: TextStyle(
-                        fontSize:15.0 ,)),
-                    )
-                  ],
-                  ),
-                 ],
-               ),
+                IconButton(
+                    icon:Icon(Icons.more_vert,size: 35.0,), onPressed: () {  } ,),
+              ],
+            )
+  
+            ],
+          ),
+          
+  
+        //ROW03
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20,10),
+            child: Container(
+              width: 350.0,
+              height: 200.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage("assests/girl.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            ),
+          ),   
+          ],
+        ),
+  
+  
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.flag,size: 30.0,color: Colors.black38,), onPressed: () {  } ,),
               ],
             ),
-
-          Column(
-            children: [
-              IconButton(
-                  icon:Icon(Icons.more_vert,size: 35.0,), onPressed: () {  } ,),
-            ],
-          )
-
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.comment,size: 25.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.share,size: 30.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+  
+  
           ],
         ),
 
-      //ROW03
+
+
+  
+  
+         Divider(
+                    color: Colors.black.withOpacity(0.1),
+                    height: 0,
+                    thickness: 3,
+                    indent: 20,
+                    endIndent: 20,
+                  
+                  ),
+          //ROW02
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:<Widget> [
+              Column(
+                children: [
+                  Row(
+                    children:<Widget> [
+                      Container(
+                      width: 40,
+                      height: 40,
+                      margin: EdgeInsets.fromLTRB(20,15,0,0),
+                      decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                            "assests/girl.png"),
+                        fit: BoxFit.fill,
+                      ),
+                      ),
+                    ),
+  
+                    Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 0,0),
+                        child: Text("Sunny Leony",style: TextStyle(
+                          fontSize:18.0 ,fontWeight: FontWeight.w700,)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 0,0),
+                        child: Text("Just Now..",style: TextStyle(
+                          fontSize:15.0 ,)),
+                      )
+                    ],
+                    ),
+                   ],
+                 ),
+                ],
+              ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.more_vert,size: 35.0,), onPressed: () {  } ,),
+              ],
+            )
+  
+            ],
+          ),
+          
+  
+        //ROW03
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20,10),
+            child: Container(
+              width: 350.0,
+              height: 200.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage("assests/girl.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            ),
+          ),   
+          ],
+        ),
+  
+  
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.flag,size: 30.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.comment,size: 25.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.share,size: 30.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+          ],
+        ),
+  
 
 
 
-      ],
+
+       
+         Divider(
+                    color: Colors.black.withOpacity(0.1),
+                    height: 0,
+                    thickness: 3,
+                    indent: 20,
+                    endIndent: 20,
+                  
+                  ),
+          //ROW02
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:<Widget> [
+              Column(
+                children: [
+                  Row(
+                    children:<Widget> [
+                      Container(
+                      width: 40,
+                      height: 40,
+                      margin: EdgeInsets.fromLTRB(20,15,0,0),
+                      decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                            "assests/girl.png"),
+                        fit: BoxFit.fill,
+                      ),
+                      ),
+                    ),
+  
+                    Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 0,0),
+                        child: Text("Sunny Leony",style: TextStyle(
+                          fontSize:18.0 ,fontWeight: FontWeight.w700,)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 0,0),
+                        child: Text("Just Now..",style: TextStyle(
+                          fontSize:15.0 ,)),
+                      )
+                    ],
+                    ),
+                   ],
+                 ),
+                ],
+              ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.more_vert,size: 35.0,), onPressed: () {  } ,),
+              ],
+            )
+  
+            ],
+          ),
+          
+  
+        //ROW03
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20,10),
+            child: Container(
+              width: 350.0,
+              height: 200.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage("assests/girl.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            ),
+          ),   
+          ],
+        ),
+  
+  
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.flag,size: 30.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.comment,size: 25.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+            Column(
+              children: [
+                IconButton(
+                    icon:Icon(Icons.share,size: 30.0,color: Colors.black38,), onPressed: () {  } ,),
+              ],
+            ),
+  
+  
+  
+          ],
+        ) 
+  
+        ],
+      ),
     ),
   );
 }
