f0ba2c221e7f2efa6f749f3a8c9328eec29d4fb0
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-07-17 21:51:42 +0530

    form validated

--- lib/login.dart
+++ lib/login.dart
@@ -11,6 +11,8 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
 
 final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+var _uname;
+var _pwd;
 
 Widget _username() {
   return Container(
@@ -18,7 +20,7 @@ Widget _username() {
                 padding: const EdgeInsets.fromLTRB(30,10,30,10),
                 child: Card(
                     shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30), // if you need this
+                    borderRadius: BorderRadius.circular(100), // if you need this
                     side: BorderSide(
                       color: Colors.white.withOpacity(0.5),
                       width: 1,
@@ -29,16 +31,32 @@ Widget _username() {
                     padding: const EdgeInsets.fromLTRB(20,0,20,0),
                       child: Column(
                         children: [
-                          TextFormField(
-                            cursorColor: Colors.white,
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Username',
-                              hintStyle: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
-                              
-                            ),
-                            )
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0,0,0,5),
+                            child: TextFormField(
+                              cursorColor: Colors.white,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Username',
+                                hintStyle: TextStyle(
+                                  color: Colors.white, 
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 18),
+                                
+                              ),
+                              validator: (text){
+                                if(text!.isEmpty){
+                                  return 'Field Required !';
+                                }
+                                return null;
+                              },
+
+                              onSaved: (text){
+                                _uname = text!;
+                              },
+                              ),
+                          )
                         ],
                       ),
                   ), 
@@ -55,7 +73,7 @@ Widget _password() {
                 padding: const EdgeInsets.fromLTRB(30,10,30,10),
                 child: Card(
                     shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30), // if you need this
+                    borderRadius: BorderRadius.circular(100), // if you need this
                     side: BorderSide(
                       color: Colors.white.withOpacity(0.5),
                       width: 1,
@@ -66,17 +84,34 @@ Widget _password() {
                     padding: const EdgeInsets.fromLTRB(20,0,20,0),
                       child: Column(
                         children: [
-                          TextFormField(
-                            obscureText: true,
-                            cursorColor: Colors.white,
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Password',
-                              hintStyle: TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
-                              
-                            ),
-                            )
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0,0,0,5),
+                            child: TextFormField(
+                              obscureText: true,
+                              cursorColor: Colors.white,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Password',
+                                hintStyle: TextStyle(
+                                  color: Colors.white, 
+                                  fontStyle: FontStyle.italic,
+                                  fontSize: 18),
+                                
+                              ),
+
+                              validator: (text){
+                                if(text!.isEmpty){
+                                  return 'Field Required !';
+                                }
+                                return null;
+                              },
+
+                              onSaved: (text){
+                                _pwd = text!;
+                              },
+                              ),
+                          )
                         ],
                       ),
                   ), 
@@ -88,37 +123,56 @@ Widget _password() {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Colors.blue.shade900,
-            Color.fromRGBO(128,0,0,0.9),
-          ]
-        )
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("My First",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 30.0,),),
-             SizedBox(height: 10.0,),
-            Text("Flutter Application",style: TextStyle(fontSize: 15,color: Colors.white70,letterSpacing: 7),),
-            SizedBox(height: 40,),
+    body: SingleChildScrollView(
+      child: Container(
+        height:MediaQuery.of(context).size.height * 1,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.blue.shade900,
+              Color.fromRGBO(128,0,0,0.9),
+            ]
+          )
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("My First",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w700,fontSize: 30.0,),),
+               SizedBox(height: 10.0,),
+              Text("Flutter Application",style: TextStyle(fontSize: 15,color: Colors.white70,letterSpacing: 7),),
+              SizedBox(height: 40,),
+    
+              Form(
+                key: _formKey,
+                child:Column(
+                  children: [
+                    _username(),
+                    _password(),
 
-            Form(
-              key: _formKey,
-              child:Column(
-                children: [
-                  _username(),
-                  _password()
-                ],
-              )
+                    SizedBox(
+                      height: 40,
+                    ),
 
-            ),
-          ],
+                    Container(
+                      child: RaisedButton(
+                        child: Text('Log In'),
+                        onPressed: (){
+                          if(_formKey.currentState!.validate()){
+                            _formKey.currentState!.save();
+                            print(_uname);
+                          }
+                          
+                        },),
+                    )
+                  ],
+                )
+    
+              ),
+            ],
+          ),
         ),
       ),
     ),
