4b1524910d5cfd702a8378260decb753cf91b0cf
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-07-22 23:23:54 +0530

    A bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
 @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 5), ()
+    Timer(Duration(seconds: 3), ()
     {
       Navigator.push(context, 
       MaterialPageRoute(builder: (context)=> MyHome()),
