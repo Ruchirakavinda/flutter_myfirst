74a13f7d41acabf81c8f6963ad79fdcdae626621
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-07-26 21:25:02 +0530

    stories row modified and vertical list added

--- lib/tab2.dart
+++ lib/tab2.dart
@@ -14,59 +14,135 @@ class _Tab6State extends State<Tab6> {
       body: Container(
         child: SingleChildScrollView(
           child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children:<Widget> [
-                            Column(
-                              children:<Widget> [
-                                Container(
-                                  height: 30.0,
-                                  margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                                  child:Text(
-                                    "News Feed",
-                                    style: TextStyle(
-                                      fontSize:25.0 ,fontWeight: FontWeight.w700,
-                                      ),
-                                      )
-                                      ),
-                                      
-                              ],
-                            ),
-                          Container(
-                            height:50.0,
-                              margin: EdgeInsets.all(10.0),
-                              child: Column(
+              Container(
+                child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children:<Widget> [
+                              Column(
                                 children:<Widget> [
-                                  FloatingActionButton(
-                                    backgroundColor: Colors.grey.withOpacity(0.2),
-                                    mini: true,
-                                    elevation: 0.0,
-                                    child: Icon(Icons.search,size: 25,color: Colors.black,),
-                                    onPressed: (){},
-                                  ),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                                    child:Text(
+                                      "News Feed",
+                                      style: TextStyle(
+                                        fontSize:25.0 ,fontWeight: FontWeight.w700,
+                                        ),
+                                        )
+                                        ),
+                                        
                                 ],
                               ),
-                            ),
+                            Container(
+                                margin: EdgeInsets.all(10.0),
+                                child: Column(
+                                  children:<Widget> [
+                                    FloatingActionButton(
+                                      backgroundColor: Colors.grey.withOpacity(0.2),
+                                      mini: true,
+                                      elevation: 0.0,
+                                      child: Icon(Icons.search,size: 25,color: Colors.black,),
+                                      onPressed: (){},
+                                    ),
+                                  ],
+                                ),
+                              ),
 
-                          ],
-                          ),
+                            ],
+                ),
+              ),
+
+              Container(
+                height: 10,
+                color: Colors.grey[300],
+              ),
 
               SizedBox(
                 height:200,
                 child: ListView.builder(
                   itemCount: 10,
                   scrollDirection:Axis.horizontal,
-                  itemBuilder:(context,index) => Container(
-                    width: 110,
-                    margin: EdgeInsets.all(5),
-                    child: Center(
-                      child:Text("Card $index"),
+                  itemBuilder:(context,index) => Row(
+                    children: [
+
+                      Container(
+                        width: index==0?110:0.0,
+                        margin: EdgeInsets.fromLTRB(10,10, index==0?10:0 ,10),
+                        child: Center(
+                          child:Column(
+                            children: [
+                              Text("")
+                            ],
+                          ),
+                          ),
+                        color: Colors.black,
                       ),
-                    color: Colors.teal[800],
+
+
+                      Container(
+                        width: 110,
+                        margin: EdgeInsets.fromLTRB(0,10,0,10),
+                        child: Center(
+                          child:Column(
+                            children: [
+                              Text("Card dgdgfdh $index"),
+                              Text("Card dgdgfdh $index"),
+                              Text("Card dgdgfdh $index"),
+                            ],
+                          ),
+                          ),
+                        color: Colors.teal[800],
+                      ),
+                    ],
                   ) 
                   ),
+              ),
+
+              Container(
+                height: 10,
+                color: Colors.grey[300],
+              ),
+
+
+              Flexible(
+                child:ListView.builder(
+                  itemCount: 20,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder:(context,index)=>ListTile(
+
+                    // leading: Text("hi $index"),
+                    leading:Container(
+                          width:45.0,
+                          height:45.0,
+                          margin: EdgeInsets.fromLTRB(0,0,0,0),
+                          decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                          image: AssetImage('assests/girl.png'),
+                                fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        onTap: (){},
+                        title: Text("gftgh",style: TextStyle(fontWeight: FontWeight.w500),),
+                        subtitle: Text("hgh"),
+                        trailing:Column(
+                        children: [
+                          IconButton(
+                            icon:Icon(Icons.more_horiz,size: 35.0,), onPressed: () {  } ,
+                            ),
+                          ],
+                        ),
+
+                  )
+                  ) 
               )
+
+
+
+
             ],
           ),),
       ),
